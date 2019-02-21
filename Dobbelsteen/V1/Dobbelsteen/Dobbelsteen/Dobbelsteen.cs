74fbab33f5b1124c41ca46c643e99f8afc77a9b9
Gino Otten <contact@ginootten.nl>
2019-02-21 12:25:06 +0100

    Upload DobbelSteen

---
+++ Dobbelsteen/V1/Dobbelsteen/Dobbelsteen/Dobbelsteen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dobbelsteen
+{
+    class Dobbelsteen
+    {
+        //  Properties
+        private int ogen = 0;
+
+        //  Constructor ( Geen Type, zelfde naam als de class )
+        public Dobbelsteen()
+        {
+        }
+
+        //  Methodes ( Werkworden (waar je ik voor kan zetten ))
+        public int Gooi()
+        {
+            Random random = new Random();
+            Ogen = random.Next(1,7);
+
+            return Ogen;
+        }
+
+        //  Gettersetter
+        private int Ogen
+        {
+            get { return this.ogen;  }
+            set { this.ogen = value; }
+        }
+    }
+}
---
+++ Dobbelsteen/V1/Dobbelsteen/Dobbelsteen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dobbelsteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object
+            Dobbelsteen steen1 = new Dobbelsteen();
+            int getal = steen1.Gooi();
+
+            Console.WriteLine("getal=" + getal);
+            Console.Read();
+        }
+    }
+}
